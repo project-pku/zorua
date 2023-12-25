a45a4f347e4d842488afc3bdd130f7e33f22c07e
prof64 <80442784+prof64@users.noreply.github.com>
2023-12-24 19:05:59 -0500

    Refactor lib to use traits
    A complete refactoring of the crate. Now:
    - To use a type in a zoura struct field, it must implement ZouraField.
    - To be used in a bitfield, it must implement ZouraBitField.
    - You can even implement new backing types for fields/bitfields by
    implementing the BackedField/BackedBitField traits (although this mostly
    intended for internal use).
    - bool, u1-u7 and the native uints are implemented for both kinds.
    
    Here's how it all works now:
    - All ZouraBitFields have a BitRepr type, and bit fiddling operations
    take place with that canonical representation. The same goes for
    ZouraFields and their ByteRepr types.
    - Moreover, types can implement both these traits. This means a type can
    have a Byte size (i.e. it's normal layout in rust) and a Bit size (i.e.
    how many bits it takes up when packed).
    - For example a bit takes one byte as a field, but one bit as a
    bitfield. Similarly for integers (e.g. u9 is has BitRepr of u9 and
    ByteRepr of u16), and perhaps even enums.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,66 @@ pub mod data_type {
     pub use arbitrary_int::*;
 }
 
-//Automates boilerplate for implementing Zorua on int types
-macro_rules! impl_zorua_int {
+/// Automates boilerplate for implementing ZoruaField
+/// and BackingField on built-in int types
+macro_rules! impl_backing {
     ($($ty:ty),*) => {
         $(
-            impl Zorua for $ty {
-                #[inline]
+            impl BackingField for $ty {
+                fn get_bits<T: BackingBitField, const INDEX: u32>(self) -> T
+                where Self: From<T::UnderlyingType> + TryInto<T::UnderlyingType> {
+                    T::new(
+                        ((self & (Into::<$ty>::into(T::MASK) << INDEX)) >> INDEX).try_into().unwrap_or_else(
+                            |_| {
+                                panic!("Zorua Error: The BackedBitField::MASK of type {} must be wrong",
+                                    std::any::type_name::<$ty>())
+                            }
+                        )
+                    )
+                }
+                fn set_bits<T: BackingBitField, const INDEX: u32>(&mut self, value: T)
+                where Self: From<T::UnderlyingType> + TryInto<T::UnderlyingType> {
+                    *self &= !(Into::<$ty>::into(T::MASK) << INDEX);
+                    *self |= (Into::<$ty>::into(value.get_backed())) << INDEX;
+                }
+            }
+            impl ZoruaField for $ty {
                 fn swap_bytes_mut(&mut self) {
                     *self = (*self).swap_bytes();
                 }
+            }
+        )*
+    };
+}
+
+/// Automates boilerplate for implementing ZoruaBitField
+/// and BackingBitField on arbitrary-int & built-in int types
+macro_rules! impl_bit_backing {
+    ("arbitrary", $($ty:ty),*) => {
+        $(
+            impl ZoruaBitField for $ty {
+                type BitRepr = Self;
+                fn to_repr(self) -> Self::BitRepr { self }
+                fn from_repr(value: Self::BitRepr) -> Self { value }
+            }
+            impl BackingBitField for $ty {
+                const MASK: Self::UnderlyingType = <$ty>::MASK;
+                fn get_backed(self) -> Self::UnderlyingType {
+                    self.value()
+                }
+            }
+        )*
+    };
+    ("native", $($ty:ty),*) => {
+        $(
+            impl ZoruaBitField for $ty {
+                type BitRepr = Self;
+                fn to_repr(self) -> Self::BitRepr { self }
+                fn from_repr(value: Self::BitRepr) -> Self { value }
+            }
+            impl BackingBitField for $ty {
+                const MASK: Self::UnderlyingType = <$ty>::MAX;
+                fn get_backed(self) -> Self::UnderlyingType { self }
             }
         )*
     };
@@ -21,69 +72,84 @@ pub mod prelude {
     pub use crate::data_type::*;
     pub use paste::paste;
 
-    pub trait Zorua {
+    //------------- Field trait + impls -------------
+    /// A type that can be used within a ZoruaStruct.
+    pub trait ZoruaField {
         fn swap_bytes_mut(&mut self);
 
         fn to_le_mut(&mut self) {
             //Assumption: All targets are either little or big endian.
-            #[cfg(target_endian = "big")] {
+            #[cfg(target_endian = "big")]
+            {
                 self.swap_bytes_mut();
             }
         }
 
         fn to_be_mut(&mut self) {
             //Assumption: All targets are either little or big endian.
-            #[cfg(target_endian = "little")] {
+            #[cfg(target_endian = "little")]
+            {
                 self.swap_bytes_mut();
             }
         }
     }
-    impl Zorua for () {
+
+    /// A special kind of [ZoruaField] that can house [ZoruaBitField]s.
+    pub trait BackingField: ZoruaField {
+        fn get_bits<T: BackingBitField, const INDEX: u32>(self) -> T
+        where
+            Self: From<T::UnderlyingType> + TryInto<T::UnderlyingType>;
+
+        fn set_bits<T: BackingBitField, const INDEX: u32>(&mut self, value: T)
+        where
+            Self: From<T::UnderlyingType> + TryInto<T::UnderlyingType>;
+    }
+    impl_backing!(u8, u16, u32, u64, u128);
+    impl ZoruaField for bool {
         fn swap_bytes_mut(&mut self) {}
     }
-    impl_zorua_int!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128);
-    impl<const N: usize, T: Zorua> Zorua for [T; N] {
+    impl ZoruaField for () {
+        fn swap_bytes_mut(&mut self) {}
+    }
+    impl<const N: usize, T: ZoruaField> ZoruaField for [T; N] {
         fn swap_bytes_mut(&mut self) {
             self.iter_mut().for_each(|value| {
                 value.swap_bytes_mut();
             });
         }
     }
+    //-----------------------------------------------
+
+    //----------- BitField trait + impls -----------
+    pub trait ZoruaBitField {
+        /// The smallest bit representation of this type.
+        type BitRepr: BackingBitField;
+
+        /// Returns a copy of this type as its N bit [BitRepr][ZoruaBitField].
+        fn to_repr(self) -> Self::BitRepr;
+
+        /// Packs this type into its N bit [BitRepr][ZoruaBitField].
+        fn from_repr(value: Self::BitRepr) -> Self;
+    }
+    pub trait BackingBitField: ZoruaBitField + Number {
+        const MASK: Self::UnderlyingType;
+        fn get_backed(self) -> Self::UnderlyingType;
+    }
+    impl_bit_backing!("arbitrary", u1, u2, u3, u4, u5, u6, u7);
+    impl_bit_backing!("native", u8, u16, u32, u64, u128);
+    impl ZoruaBitField for bool {
+        type BitRepr = u1;
+        fn to_repr(self) -> Self::BitRepr {
+            u1::new(if self { 1 } else { 0 })
+        }
+        fn from_repr(value: Self::BitRepr) -> Self {
+            value == u1::MAX
+        }
+    }
+    //---------------------------------------------
 
     #[macro_export]
     macro_rules! zorua {
-        //Subfield get/set implementations
-        (impl "$sf_impl", $f:ident, $ft:ty, $sfv:vis, $sf:ident, $sfs:expr, bool) => {
-            paste! {
-                $sfv fn $sf(&self) -> bool {
-                    let bitmask = 1 << $sfs;
-                    (self.$f & bitmask) != 0
-                }
-                $sfv fn [<set_ $sf>](&mut self, val: bool) {
-                    let bitmask: $ft = 1 << $sfs;
-                    if val {
-                        self.$f |= Into::<$ft>::into(bitmask);
-                    } else {
-                        self.$f &= Into::<$ft>::into(!bitmask);
-                    }
-                }
-            }
-        };
-        (impl "$sf_impl", $f:ident, $ft:ty, $sfv:vis, $sf:ident, $sfs:expr, $sft:ty) => {
-            paste! {
-                $sfv fn $sf(&self) -> $sft {
-                    $sft::new(
-                        ((self.$f & (($sft::MASK as $ft) << $sfs)) >> $sfs)
-                            as <$sft as Number>::UnderlyingType,
-                    )
-                }
-                $sfv fn [<set_ $sf>](&mut self, val: $sft) {
-                    self.$f &= Into::<$ft>::into(!(($sft::MASK as $ft) << $sfs));
-                    self.$f |= Into::<$ft>::into((val.value() << $sfs) as $ft);
-                }
-            }
-        };
-
         //Regular struct macro
         //Generic support courtesy of: https://stackoverflow.com/a/61189128/10910105
         (
@@ -103,10 +169,19 @@ pub mod prelude {
                 // Generate the impl block
                 impl$(<$($lt$(:$clt$(+$dlt)*)?),+>)? $struct_name$(<$($lt),+>)? {
                     $($($(
-                        zorua!(impl "$sf_impl", $f, $ft, $sfv, $sf, $sfs, $sft);
+                        paste! {
+                            $sfv fn $sf(&self) -> $sft {
+                                let bit_repr = self.$f.get_bits::<<$sft as ZoruaBitField>::BitRepr, $sfs>();
+                                <$sft as ZoruaBitField>::from_repr(bit_repr)
+                            }
+                            $sfv fn [<set_ $sf>](&mut self, val: $sft) {
+                                let bit_repr = val.to_repr();
+                                self.$f.set_bits::<<$sft as ZoruaBitField>::BitRepr, $sfs>(bit_repr);
+                            }
+                        }
                     )+)?)*
                 }
-                impl$(<$($lt$(:$clt$(+$dlt)*)?),+>)? Zorua for $struct_name$(<$($lt),+>)? {
+                impl$(<$($lt$(:$clt$(+$dlt)*)?),+>)? ZoruaField for $struct_name$(<$($lt),+>)? {
                     fn swap_bytes_mut(&mut self) {
                         $(self.$f.swap_bytes_mut();)*
                     }
@@ -124,7 +199,7 @@ pub mod prelude {
             #[derive(Debug, PartialEq, Clone)]
             $sv struct $struct_name$(<$($lt:tt$(:$clt:tt$(+$dlt:tt)*)?),+>)? ($fv $ft);
 
-            impl$(<$($lt$(:$clt$(+$dlt)*)?),+>)? Zorua for $struct_name$(<$($lt),+>)? {
+            impl$(<$($lt$(:$clt$(+$dlt)*)?),+>)? ZoruaField for $struct_name$(<$($lt),+>)? {
                 #[inline]
                 fn swap_bytes_mut(&mut self) {
                     self.0.swap_bytes_mut();
@@ -143,7 +218,7 @@ pub mod prelude {
             #[derive(Debug, PartialEq, Clone)]
             $sv struct $struct_name<const $N : $Nt> ($fv $ft);
 
-            impl<const $N: $Nt> Zorua for $struct_name<$N> {
+            impl<const $N: $Nt> ZoruaField for $struct_name<$N> {
                 #[inline]
                 fn swap_bytes_mut(&mut self) {
                     self.0.swap_bytes_mut();
