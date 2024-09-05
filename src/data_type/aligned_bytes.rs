3e341dccc723d20dd5c2ad6b461dfef7ae1bb9f7
prof64 <80442784+prof64@users.noreply.github.com>
2024-09-05 15:53:21 -0400

    Replace Alignment impl w/ elain crate's
    Now using the elain crate to deal with ensuring the alignment of the
    AlignedBytes type.

--- src/data_type/aligned_bytes.rs
+++ src/data_type/aligned_bytes.rs
@@ -1,22 +1,29 @@
-pub use alignment::*;
+use elain::{Align, Alignment};
 use std::ops::{Deref, DerefMut};
 
-/// A smart pointer for a length `N` array that guarantees its [Alignment].
+/// A smart pointer for a length `N` array that guarantees an alignment of `ALIGN`.
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct AlignedBytes<A: Alignment, const N: usize> {
-    _alignment: [A; 0],
+pub struct AlignedBytes<const ALIGN: usize, const N: usize>
+where
+    Align<ALIGN>: Alignment,
+{
+    _alignment: [Align<ALIGN>; 0],
     value: [u8; N],
 }
 
-impl<A: Alignment, const N: usize> AlignedBytes<A, N> {
-    pub const fn new() -> AlignedBytes<A, N> {
+impl<const ALIGN: usize, const N: usize> AlignedBytes<ALIGN, N>
+where
+    Align<ALIGN>: Alignment,
+{
+    /// For larger allocations better suited to the heap, use [AlignedBytes::new_boxed].
+    pub const fn new() -> AlignedBytes<ALIGN, N> {
         AlignedBytes {
             _alignment: [],
             value: [0; N],
         }
     }
 
-    pub const fn from_bytes(bytes: [u8; N]) -> AlignedBytes<A, N> {
+    pub const fn from_bytes(bytes: [u8; N]) -> AlignedBytes<ALIGN, N> {
         AlignedBytes {
             _alignment: [],
             value: bytes,
@@ -24,7 +31,10 @@ impl<A: Alignment, const N: usize> AlignedBytes<A, N> {
     }
 }
 
-impl<A: Alignment, const N: usize> Default for AlignedBytes<A, N> {
+impl<const ALIGN: usize, const N: usize> Default for AlignedBytes<ALIGN, N>
+where
+    Align<ALIGN>: Alignment,
+{
     fn default() -> Self {
         Self::new()
     }
@@ -33,7 +43,10 @@ impl<A: Alignment, const N: usize> Default for AlignedBytes<A, N> {
 // -------------------------
 // Deref Impls
 // -------------------------
-impl<A: Alignment, const N: usize> Deref for AlignedBytes<A, N> {
+impl<const ALIGN: usize, const N: usize> Deref for AlignedBytes<ALIGN, N>
+where
+    Align<ALIGN>: Alignment,
+{
     type Target = [u8; N];
 
     fn deref(&self) -> &Self::Target {
@@ -41,76 +54,11 @@ impl<A: Alignment, const N: usize> Deref for AlignedBytes<A, N> {
     }
 }
 
-impl<A: Alignment, const N: usize> DerefMut for AlignedBytes<A, N> {
+impl<const ALIGN: usize, const N: usize> DerefMut for AlignedBytes<ALIGN, N>
+where
+    Align<ALIGN>: Alignment,
+{
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.value
     }
-}
-
-mod sealed {
-    pub trait Sealed {}
-}
-
-mod alignment {
-    use super::sealed::Sealed;
-
-    /// 2-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(1))]
-    pub struct A1;
-
-    /// 2-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(2))]
-    pub struct A2;
-
-    /// 4-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(4))]
-    pub struct A4;
-
-    /// 8-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(8))]
-    pub struct A8;
-
-    /// 16-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(16))]
-    pub struct A16;
-
-    /// 32-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(32))]
-    pub struct A32;
-
-    /// 64-byte alignment
-    #[derive(Clone, Copy, Debug)]
-    #[repr(align(64))]
-    pub struct A64;
-
-    /// 128-byte alignment
-    #[derive(Clone, Copy)]
-    #[repr(align(128))]
-    pub struct A128;
-
-    /// Represents the alignment of a type, which is some power of 2.
-    pub trait Alignment: Copy + Sealed {} //Sealed makes this unimplementable
-    impl Alignment for A1 {}
-    impl Alignment for A2 {}
-    impl Alignment for A4 {}
-    impl Alignment for A8 {}
-    impl Alignment for A16 {}
-    impl Alignment for A32 {}
-    impl Alignment for A64 {}
-    impl Alignment for A128 {}
-
-    impl Sealed for A1 {}
-    impl Sealed for A2 {}
-    impl Sealed for A4 {}
-    impl Sealed for A8 {}
-    impl Sealed for A16 {}
-    impl Sealed for A32 {}
-    impl Sealed for A64 {}
-    impl Sealed for A128 {}
 }
--- src/data_type/mod.rs
+++ src/data_type/mod.rs
@@ -2,6 +2,7 @@ mod aligned_bytes;
 mod fallible;
 mod unaligned;
 pub use aligned_bytes::*;
+pub use elain::{Align, Alignment};
 pub use fallible::*;
 pub use unaligned::*;
 pub use ux2::{u1, u2, u3, u4, u5, u6, u7}; // < u8
