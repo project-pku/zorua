bcb919caef2013388d1e8e533ae967afa93284ae
prof64 <80442784+prof64@users.noreply.github.com>
2024-09-09 14:36:24 -0400

    Refactor Fallible struct
    Derived previously manually implemented traits.

--- src/data_type/fallible.rs
+++ src/data_type/fallible.rs
@@ -1,11 +1,7 @@
 use crate::traits::{BackingBitField, BackingField, ZoruaBitField, ZoruaField};
-use std::{
-    fmt::{Debug, Formatter},
-    marker::PhantomData,
-    mem::transmute_copy,
-};
+use std::{marker::PhantomData, mem};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Fallible<T: ZoruaFallible<B>, B> {
     _marker: PhantomData<T>,
     pub value: B,
@@ -35,7 +31,7 @@ impl<T: ZoruaFallible<B>, B> Fallible<T, B> {
     pub fn from_value(value: T) -> Fallible<T, B> {
         Fallible {
             _marker: Default::default(),
-            value: unsafe { transmute_copy(&value) },
+            value: unsafe { mem::transmute_copy(&value) },
         }
     }
 
@@ -70,17 +66,5 @@ impl<T: ZoruaFallible<B>, B: BackingBitField> ZoruaBitField for Fallible<T, B> {
 impl<T: ZoruaFallible<B>, B> From<T> for Fallible<T, B> {
     fn from(value: T) -> Self {
         Self::from_value(value)
-    }
-}
-
-impl<T: ZoruaFallible<B>, B: PartialEq> PartialEq for Fallible<T, B> {
-    fn eq(&self, other: &Self) -> bool {
-        self.value == other.value
-    }
-}
-
-impl<T: ZoruaFallible<B>, B: Debug> Debug for Fallible<T, B> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.value.fmt(f)
     }
 }
