5237f2ffbe9585268b6a93b5e0a6b0705e99b59a
prof64 <80442784+prof64@users.noreply.github.com>
2025-05-18 14:35:34 -0400

    Migrate to Rust 2024 edition
    Just had to fix 2 minor linting warnings

--- src/data_type/mod.rs
+++ src/data_type/mod.rs
@@ -6,4 +6,4 @@ pub use elain::{Align, Alignment};
 pub use fallible::*;
 pub use unaligned::*;
 pub use ux2::{u1, u2, u3, u4, u5, u6, u7}; // < u8
-pub use ux2::{u10, u11, u12, u13, u14, u15, u9}; // < u16
+pub use ux2::{u9, u10, u11, u12, u13, u14, u15}; // < u16
