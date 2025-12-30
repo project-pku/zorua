898b368f0fb73892ee6e57de583ca81d7c580194
prof64 <80442784+prof64@users.noreply.github.com>
2025-12-30 11:58:28 -0500

    [feat] Create ZoruaNative trait
    Co-authored-by: antigravity <antigravity@google.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod data_type;
+pub mod native;
 pub mod traits;
 
 #[macro_use]
 pub mod prelude {
     pub use crate::data_type::*;
+    pub use crate::native::ZoruaNative;
     pub use crate::traits::*;
     pub use paste::paste;
 
