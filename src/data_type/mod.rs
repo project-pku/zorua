0575a39f2d3a180a04d60a421d8a484e9781aa42
prof64 <80442784+prof64@users.noreply.github.com>
2025-12-31 08:15:20 -0500

    [ref] Remove Unaligned and Fallible
    Co-authored-by: antigravity <antigravity@google.com>

--- src/data_type/mod.rs
+++ src/data_type/mod.rs
@@ -1,9 +1,5 @@
 mod endian;
-mod fallible;
-mod unaligned;
 
 pub use endian::*;
-pub use fallible::*;
-pub use unaligned::*;
 pub use ux2::{u1, u2, u3, u4, u5, u6, u7}; // < u8
 pub use ux2::{u9, u10, u11, u12, u13, u14, u15}; // < u16
